28e4af40e72d8c17dc748284086882a9cb4dac41
TylerKirkpatrick <tkirkpatrick@smu.edu>
2016-03-24 17:07:45 -0500

    added breaks to file

--- newServerScript.sh
+++ newServerScript.sh
@@ -1,7 +1,37 @@
 #!/bin/bash
 
-clear
+ssh -i "productionServerKey.pem" ubuntu@ec2-52-35-167-179.us-west-2.compute.amazonaws.com -y
 
-echo "hello son"
+sudo apt-get update
 
-clear
+sudo apt-get install lamp-server^
+
+curl -sS https://getcomposer.org/installer | php
+
+sudo mv composer.phar /usr/local/bin/composer
+
+composer create-project slim/slim-skeleton /var/www/grupr1.dev
+
+sudo composer create-project slim/slim-skeleton /var/www/grupr1.dev
+
+sudo vim /etc/apache2/sites-available/000-default.conf
+
+read -n1 -r -p "Press any key to continue..." key
+
+sudo a2enmod rewrite
+
+sudo service apache2 restart
+
+touch 00-php.ini
+
+vim 00-php.ini 
+
+read -n1 -r -p "Press any key to continue..." key
+
+sudo cp 00-php.ini /etc/php5/apache2/conf.d/
+
+sudo service apache2 restart
+
+cd /var/www/grupr1.dev/
+
+sudo chmod 777 ./logs -R
